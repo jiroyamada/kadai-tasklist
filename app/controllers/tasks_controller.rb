a64e2f6a1a0a38be9cdc4c3879b6053f15f98fa0
taro.yamada <poirot343@outlook.jp>
2018-08-20 09:37:34 +0000

    ititaitasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,61 +1,65 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :task_find,only: [:show,:edit,:update,:destroy]
+  
+  
   def index
-     @tasks = Task.all.page(params[:page])
+    @user = current_user
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end
-
+  
   def show
-    set_task
   end
 
+  
   def new
     @task = Task.new
   end
-
+  
   def create
-        @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'tasks/index'
     end
+    
   end
 
+  
   def edit
-    set_task
   end
-
+  
   def update
-    set_task
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
 
+
   def destroy
-    set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
-
-  # Strong Parameter
   def task_params
-    params.require(:task).permit(:content,:status)
+    params.require(:task).permit(:content)
   end
+  
+  def task_find
+    @task  = Task.find(params[:id])
+  end
+  
 end
